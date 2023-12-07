611ac5b296daf430c94e8d68bf4c2cf5aa974bb7
Yagurchik <gredseven@mail.ru>
2023-12-07 18:29:41 +0300

    Homework 5

---
+++ HW5/Factorial.cs
@@ -0,0 +1,23 @@
+﻿namespace HW5_3
+{
+    internal class Factorial
+    {
+        static void Main(string[] args)
+        {
+            int fac = 1;
+            bool flag = Int32.TryParse(Console.ReadLine(), out int n);
+            if (flag)
+            {
+                for (int i = 1; i <= n; i++) 
+                {
+                    fac = fac * i;
+                }
+                Console.WriteLine(fac);
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
---
+++ HW5/Fibachi.cs
@@ -0,0 +1,26 @@
+﻿namespace HW5_4
+{
+    internal class Fibachi
+    {
+        static void Main(string[] args)
+        {
+            int fib = 1;
+            int a = 0;
+            bool flag = Int32.TryParse(Console.ReadLine(), out int n);
+            if (flag)
+            {
+                for (int i = 3; i <= n; i++)
+                {
+                    int c = fib + a;
+                    a = fib;
+                    fib = c;
+                }
+                Console.WriteLine(fib);
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
