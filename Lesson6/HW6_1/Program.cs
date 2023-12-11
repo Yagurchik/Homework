c2cb0f9f859ca041f3e5acaeb13f5f27231ba112
Yagurchik <gredseven@mail.ru>
2023-12-12 00:19:17 +0300

    Homework 6, ex1. Я долго пытался сделать но у меня так и не вышло. Просьба сказать что пошло не так, и где можно было бы сделать иначе.

---
+++ Lesson6/HW6_1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace HW6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите число строк:");
+            string[] members = new string[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < members.Length; i++)
+            {
+                members[i] = Console.ReadLine();
+            }
+            MsvAndStr(members, "word");
+            for (int i = 0; i < members.Length; i++)
+            {
+                Console.Write(members[i] + " ");
+            }
+        }
+        string[] MsvAndStr(string[] members, string value)
+        {
+            if  (members == null || members.Length == 0 || value == null)
+            {
+                return members;
+            }
+            else if ((members.Contains(null)) || (members.Contains(value)))
+            {
+                for (int i = 0; i < members.Length; i++)
+                {
+                    if (members[i] == null)
+                    {
+                        members[i] = value;
+                        return members;
+                    }
+                }
+            }
+            else
+            {
+                Array.Resize(ref members, members.Length + 1);
+                members[members.Length - 1] = value;
+                return members;
+            }
+        }
+    }
+}
